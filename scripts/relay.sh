5a9687f229cbd4961bdf27f9024a88a6d68efa43
masterial <konstantin.k.yurchenko@gmail.com>
2021-06-09 14:13:43 -0700

    Extend utility script.

--- scripts/relay.sh
+++ scripts/relay.sh
@@ -2,6 +2,49 @@
 # Bash Menu Script Example
 
 
+function_menu_lncli () {
+  PS3='Please enter your choice: '
+    options=("getinfo" "create_wallet" "wallet_balance" "channel_balance"  "list_channels" "quit")
+    select opt in "${options[@]}"
+    do
+        case $opt in
+            "getinfo")
+                cmd="lncli --lnddir=/relay/.lnd/ --macaroonpath=/relay/.lnd/data/chain/bitcoin/mainnet/admin.macaroon getinfo"
+                echo $cmd
+                $cmd            
+                ;;
+            "create_wallet")            
+                cmd="lncli --lnddir=/relay/.lnd/ --macaroonpath=/relay/.lnd/data/chain/bitcoin/mainnet/admin.macaroon newaddress p2wkh"
+                echo $cmd
+                $cmd
+                ;;
+            "wallet_balance")
+                cmd="lncli --lnddir=/relay/.lnd/ --macaroonpath=/relay/.lnd/data/chain/bitcoin/mainnet/admin.macaroon walletbalance"
+                echo $cmd
+                $cmd
+                ;;
+            "channel_balance")
+                cmd="lncli --lnddir=/relay/.lnd/ --macaroonpath=/relay/.lnd/data/chain/bitcoin/mainnet/admin.macaroon channelbalance"
+                echo $cmd
+                $cmd            
+                ;;
+            "list_channels")
+                cmd="lncli --lnddir=/relay/.lnd/ --macaroonpath=/relay/.lnd/data/chain/bitcoin/mainnet/admin.macaroon listchannels"
+                echo $cmd
+                $cmd            
+                ;;
+            "quit")
+                break
+                ;;
+            *) 
+                PS3="" # this hides the prompt
+                echo asdf | select foo in "${options[@]}"; do break; done # dummy select 
+                PS3="Please enter your choice: " # this displays the common prompt
+                ;;
+        esac
+    done
+}
+
 function_menu_logs () {
   PS3='Please enter your choice: '
     options=("log_lnd" "log_relay" "log_tor" "quit")
@@ -38,40 +81,20 @@ function_menu_logs () {
 
 function_menu_main () {
   PS3='Please enter your choice: '
-    options=("create_wallet" "wallet_balance" "channel_balance" "list_channels" "logs" "connection_string" "getinfo" "check_tor" "quit")
+    options=("lnclie" "logs" "connection_string" "check_tor" "quit")
     select opt in "${options[@]}"
     do
         case $opt in
-            "create_wallet")            
-                cmd="lncli --lnddir=/relay/.lnd/ --macaroonpath=/relay/.lnd/data/chain/bitcoin/mainnet/admin.macaroon newaddress p2wkh"
-                echo $cmd
-                $cmd
+            "lncli")
+                function_menu_lncli     
                 ;;
-            "wallet_balance")
-                cmd="lncli --lnddir=/relay/.lnd/ --macaroonpath=/relay/.lnd/data/chain/bitcoin/mainnet/admin.macaroon walletbalance"
-                echo $cmd
-                $cmd
-                ;;
-            "channel_balance")
-                cmd="lncli --lnddir=/relay/.lnd/ --macaroonpath=/relay/.lnd/data/chain/bitcoin/mainnet/admin.macaroon channelbalance"
-                echo $cmd
-                $cmd            
-                ;;
-            "list_channels")
-                cmd="lncli --lnddir=/relay/.lnd/ --macaroonpath=/relay/.lnd/data/chain/bitcoin/mainnet/admin.macaroon listchannels"
-                echo $cmd
-                $cmd            
-                ;;
+
             "logs")
                 function_menu_logs       
                 ;;
+
             "connection_string")
                 cmd="cat /relay/connection_string.txt "
-                echo $cmd
-                $cmd            
-                ;;
-            "getinfo")
-                cmd="lncli --lnddir=/relay/.lnd/ --macaroonpath=/relay/.lnd/data/chain/bitcoin/mainnet/admin.macaroon getinfo"
                 echo $cmd
                 $cmd            
                 ;;
@@ -80,6 +103,18 @@ function_menu_main () {
                 cmd="curl --socks5 localhost:9050 --socks5-hostname localhost:9050 -s https://check.torproject.org/ | cat | grep -m 1 Congratulations | xargs"
                 echo $cmd
                 $cmd            
+                ;;
+
+            "reset_sphinx_db")
+                cmd="rm -rf sphinx.db"
+                echo $cmd
+                $cmd
+                cmd="touch sphinx.db"
+                echo $cmd
+                $cmd  
+                cmd="supervisorctl restart relay"
+                echo $cmd
+                $cmd      
                 ;;
 
             "quit")
